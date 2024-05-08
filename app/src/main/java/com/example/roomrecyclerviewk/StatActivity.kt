c9d4d0f601142c7e00af5b2822a9af356991240b
Ivan Petrushin <ivan.kiel@gmail.com>
2024-05-08 14:23:22 +0800

    Room basics added

---
+++ app/src/main/java/com/example/roomrecyclerviewk/StatActivity.kt
@@ -0,0 +1,10 @@
+package com.example.roomrecyclerviewk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
