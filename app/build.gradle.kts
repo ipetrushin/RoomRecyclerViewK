30448b0ac8655ab3a15cc73ec2f1b06459ebde7c
Ivan Petrushin <ivan.kiel@gmail.com>
2024-05-08 11:30:34 +0800

    ksp version fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.roomrecyclerviewk"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.roomrecyclerviewk"
